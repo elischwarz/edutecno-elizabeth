fda6de9cc44d65c1b6b595727b2b1ab78b468fdf
elizabeth <elischwarz@gmail.com>
2021-05-15 21:42:53 -0400

    Se corrigió color y posición de iconos de footer

---
+++ Prueba Chile 1/assets/js/main.js
@@ -0,0 +1,15 @@
+$(documento).ready(function(){
+
+    $("a").click(function(event){
+        event.preventDefault()
+
+        var gato = this.hash;
+
+        $("html").animate({
+
+            scrolltop: $(gato).offset().top
+        },
+        800
+        );
+    });
+});
