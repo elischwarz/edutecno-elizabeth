60a1e899caab0ba34cdcd81f4cf76d59f1f04e13
elizabeth <elischwarz@gmail.com>
2021-07-25 22:17:00 -0400

    actualizar desafios

---
+++ ToDoList/src/app.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+<div id="app">
+    Tarea
+<input type="text" v-model="name"/>
+<p> LISTA {{name}} </p>
+    <div v-if="mostrar">
+{{ message }}
+</div>
+</div>
+</template>
+<script>
+export default {
+name: "app",
+data() {
+return {
+message: 'Hola Desafío Latam!!! 🤟🤟',
+};
+}
+};
+</script>
+
+
---
+++ ToDoList/src/hellovue.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="hellovue">
+{{message}}
+</div>
+</template>
+<script>
+export default {
+name: 'HelloVue',
+data() {
+return {
+message :'LISTA',
+};
+},
+};
+</script>
+<style scoped>
+.hellovue {
+color: black;
+font-size: 20px;
+}
+</style>
